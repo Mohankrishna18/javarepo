34be02ec1c655874014368ed49db7d71df5a8d2a
mohan <Mohankrishna.madanapu@arshaa.com>
2022-11-23 16:03:02 +0530

    updated from the code

--- src/main/java/com/arshaa/gitsample/controller/useController.java
+++ src/main/java/com/arshaa/gitsample/controller/useController.java
@@ -1,5 +1,14 @@
 package com.arshaa.gitsample.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class useController {
+	@GetMapping("/helloworld")
+	public String demo() {
+		return "Hello world";
+		
+	}
 
 }
