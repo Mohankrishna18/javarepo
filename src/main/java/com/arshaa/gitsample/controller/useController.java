5de5873336d62252df0e2c3e55f68919fb0de4d3
mohan <Mohankrishna.madanapu@arshaa.com>
2022-11-25 12:26:23 +0530

    deployment is working fine

--- src/main/java/com/arshaa/gitsample/controller/useController.java
+++ src/main/java/com/arshaa/gitsample/controller/useController.java
@@ -1,11 +1,11 @@
 package com.arshaa.gitsample.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class useController {
-	@GetMapping("/helloworld")
+	// @GetMapping("/helloworld")
 	public String demo() {
 		return "Hello world  need to push";
 		
